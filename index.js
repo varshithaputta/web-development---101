ff831094a5c7e7eee44dd9f0d93f09d3906135ea
varshithaputta <116485507+varshithaputta@users.noreply.github.com>
2023-11-21 20:12:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function submitForm() {
+  // Get form values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var dob = document.getElementById("dob").value;
+  var acceptTerms = document.getElementById("acceptTerms").checked;
+
+  // Perform validation (you can add more validation logic here)
+
+  // Display the submitted data (you can modify this part based on your needs)
+  alert("Submitted Data:\nName: " + name + "\nEmail: " + email + "\nPassword: " + password + "\nDOB: " + dob + "\nAccept Terms: " + acceptTerms);
+}
 
